86f03cbd37c648e22d50c01677b174affb7af06d
devin-bielejec <devin.bieleje@gmail.com>
2019-07-25 19:13:06 -0400

    I think I get setters, setters, and static about 90 percent of the way

--- assignments/staticMethods.js
+++ assignments/staticMethods.js
@@ -13,16 +13,43 @@ static methods don't go to the instances of the class you create
 */
 
 
-class Person {
-    constructor(details) {
+//devin.chicken();
+
+
+/* 
+
+get syntax bind an already existing property to a function
+
+*/
+
+
+//when would i want to calculate something about a property on my class
+
+class Temperature {
+    constructor(celsius) {
+      this.celsius = celsius;
     }
-
-    static chicken() {
-        console.log("Chicken");
+    get fahrenheit() {
+      return this.celsius * 1.8 + 32;
     }
-}
+    set fahrenheit(value) {
+      this.celsius = (value - 32) / 1.8;
+    }
+  
+    static fromFahrenheit(value) {
+      return new Temperature((value - 32) / 1.8);
+    }
+  }
+  
+  let temp = new Temperature(22);
+  console.log(temp.fahrenheit);
+  //getting a fahrenheit property from the function that is called inside, so I already have a property instead of having to do temp.fahrenheit = temp.fahrenheit(); GETTING PROPERTY
+  
+  temp.fahrenheit = 86;
+  console.log(temp.celsius);
+  //SETTING value to 86, so then inside the function runs and changes the this.celcius value
 
-Person.chicken();
 
-let devin = new Person;
-devin.chicken();
+  //Static allows us to create an instance differently
+  let temp2 = Temperature.fromFahrenheit(212);
+  console.log(temp2.celsius)
